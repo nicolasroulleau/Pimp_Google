42b7b88d798eeb77eae493de946c6b2af7323562
Nicolas Roulleau <nicolas.roulleau@gmail.com>
2018-01-11 20:02:10 +0100

    1er commit Pimp Google

---
+++ main.js
@@ -0,0 +1,17 @@
+let monBouton = document.getElementById('affichage')
+monBouton.onclick = function() 
+{
+	document.getElementById('bloc_centre').style.display='block';
+	document.getElementById('affichage').style.display='none';
+}
+
+let maRecherche = document.getElementById('search')
+maRecherche.onclick = function()
+{
+	let val_input = document.getElementById('input').value;
+	val_input.replace(/ /g,"+")
+	maRecherche.setAttribute('href',"http://www.google.com/#q="+val_input);
+
+}
+
+setTimeout(function() {alert('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please')}, 10000);
